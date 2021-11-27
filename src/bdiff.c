8c61adf0c2aea9123f6d8bad438cbcd3aab9e075
Finn Rayment <finn@rayment.fr>
2021-11-27 14:25:42 +0100

    Added -g option for byte grouping and updated manpage.

--- src/bdiff.c
+++ src/bdiff.c
@@ -21,9 +21,10 @@ usage(char *argv)
 	fprintf(stderr, "Compare two binary files byte by byte.\n");
 	fprintf(stderr, "\nOptions:\n");
 	fprintf(stderr, "    -c      show coloured output\n");
+	fprintf(stderr, "    -g n    num. bytes per output group (default 2)\n");
 	fprintf(stderr, "    -h      print this help message\n");
 	fprintf(stderr,
-	        "    -l      set number of lines to output, or < 1 for infinite\n");
+	        "    -l n    set number of lines to output, or < 1 for infinite\n");
 	fprintf(stderr, "    -v      print the program version\n");
 	return EXIT_FAILURE;
 }
@@ -41,24 +42,32 @@ void
 print_buffer(unsigned char buf[BLOCK_WIDTH],
              int flags[BLOCK_WIDTH],
              int color,
+             int group,
              int dir)
 {
-	int i;
+	int i, j;
 	char c;
 
 	if (dir > 0)
 		fputs("> ", stdout);
 	else
 		fputs("< ", stdout);
-	for (i = 0; i < BLOCK_WIDTH; ++i)
+	for (i = j = 0; i < BLOCK_WIDTH; ++i)
 	{
 		if (color && *(flags+i))
 			fputs("\x1b[1;31m", stdout);
 		else
 			fputs("\x1b[0m", stdout);
 		fprintf(stdout, "%02x", *(buf+i));
-		if (i < BLOCK_WIDTH - 1)
+		if (i < BLOCK_WIDTH - 1 && j == group - 1)
+		{
 			fputc(' ', stdout);
+			j = 0;
+		}
+		else
+		{
+			++j;
+		}
 	}
 	fputs("  ", stdout);
 	for (i = 0; i < BLOCK_WIDTH; ++i)
@@ -82,18 +91,20 @@ print_block(unsigned char bufa[BLOCK_WIDTH],
             unsigned char bufb[BLOCK_WIDTH],
             int flags[BLOCK_WIDTH],
             size_t block,
-            int color)
+            int color,
+            int group)
 {
 	fprintf(stdout, "%ld-%ld:\n", block*BLOCK_WIDTH+1, (block+1)*BLOCK_WIDTH);
-	print_buffer(bufa, flags, color, -1);
-	print_buffer(bufb, flags, color, 1);
+	print_buffer(bufa, flags, color, group, -1);
+	print_buffer(bufb, flags, color, group, 1);
 }
 
 int
 run(char *file_a,
     char *file_b,
     ssize_t limit,
-    int color)
+    int color,
+    int group)
 {
 	FILE *fa, *fb;
 	unsigned char bufa[BLOCK_WIDTH], bufb[BLOCK_WIDTH];
@@ -137,7 +148,7 @@ run(char *file_a,
 		{
 			++hits;
 			if (hits < limit || limit <= 0)
-				print_block(bufa, bufb, flags, block, color);
+				print_block(bufa, bufb, flags, block, color, group);
 		}
 		++block;
 		if (feof(fa) || feof(fb))
@@ -159,20 +170,26 @@ int
 main(int argc,
      char **argv)
 {
-	int c, color;
+	int c, color, group;
 	ssize_t limit;
 	char *endptr;
 
 	color = 0;
+	group = 2;
 	opterr = 0;
 	limit = LIMIT;
 
-	while ((c = getopt(argc, argv, "chl:v")) != -1)
+	while ((c = getopt(argc, argv, "cg:hl:v")) != -1)
 	{
 		switch (c)
 		{
 		case 'c':
 			color = 1;
+			break;
+		case 'g':
+			group = (int) strtol(optarg, &endptr, 10);
+			if (endptr != optarg+strlen(optarg) || group < 0)
+				return usage(*argv);
 			break;
 		case 'h':
 			usage(*argv);
@@ -199,6 +216,6 @@ main(int argc,
 	if (argc - optind != 2)
 		return usage(*argv);
 
-	return run(*(argv+optind), *(argv+optind+1), limit, color);
+	return run(*(argv+optind), *(argv+optind+1), limit, color, group);
 }
 
